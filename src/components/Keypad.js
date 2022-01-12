37c57ac97c6976a3544e3497f9141db051751bb5
Matthew Phillips <phillips.matthew829@gmail.com>
2022-01-12 12:10:10 -0600

    commit tree 4ec5bdc08e1eeeb0418103ff5a7db56b4cd2be5f

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+
+function Keypad(){
+    function word(){
+        console.log("Entering password...");
+    }
+    return <input type="password" onChange={word} />
+}
+
+export default Keypad;
